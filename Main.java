cbf115c8fa8ca288a55c5305f0561877d0703292
JavierAguila10 <156393279+JavierAguila10@users.noreply.github.com>
2024-02-02 21:40:45 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,33 @@
-import JAVIER10.Carro;
 
-public class Main{
-    public static void main(String[] args){
-        //instancias de los objetos
+import Javier1000.Temperatura;
 
-        Carro carro1 = new Carro("Nissan","Skyline",1990,55000);
-        Carro carro2 = new Carro("Mazda","CX-3", 2017, 60660 );
-        Carro carro3 = new Carro("Nissan", "Frontier", 2023, 78780);
-        //Mostrar los atributos del objeto
-        System.out.println(carro1.getMarca());
-        System.out.println(carro1.getModelo());
-        System.out.println(Carro.getNumCoche());
-
-        System.out.println(carro2.getMarca());
-        System.out.println(carro2.getModelo());
-        System.out.println(Carro.getNumCoche());
-
-        System.out.println(carro3.getMarca());
-        System.out.println(carro3.getModelo());
-        System.out.println(Carro.getNumCoche());
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Objeto 1
+        Temperatura temperatura = new Temperatura();
+        // Usar el setter para establecer la temperatura en grados Celsius
+        temperatura.setCelsius(25.0);
+        // Objeto 2
+        Temperatura temperatura2 = new Temperatura();
+        // Usar el setter para establecer la temperatura en grados Celsius
+        temperatura2.setCelsius(32.22);
+        // Usar el getter para obtener la temperatura en grados Celsius para el primer objeto
+        double temperaturaCelsius1 = temperatura.getCelsius();
+        System.out.println("Temperatura en Celsius (Objeto 1): " +
+                temperaturaCelsius1);
+        // Usar el método para convertir de Celsius a Fahrenheit para el primer objeto
+        double temperaturaFahrenheit1 = temperatura.celsiusToFahrenheit();
+        System.out.println("Temperatura en Fahrenheit (Objeto 1): " +
+                temperaturaFahrenheit1);
+        // Usar el getter para obtener la temperatura en grados Celsius para el segundo objeto
+        double temperaturaCelsius2 = temperatura2.getCelsius();
+        System.out.println("Temperatura en Celsius (Objeto 2): " +
+                temperaturaCelsius2);
+        // Usar el método para convertir de Celsius a Fahrenheit para el segundo objeto
+        double temperaturaFahrenheit2 = temperatura2.celsiusToFahrenheit();
+        System.out.println("Temperatura en Fahrenheit (Objeto 2): " +
+                temperaturaFahrenheit2);
 
     }
 }
